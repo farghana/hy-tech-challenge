74d417056a3898955880876c12ddc88b64396197
Farghana Hussain <farghana.hussain@itconquest.com>
2018-08-20 00:30:50 -0400

    Add About section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/nav_bar'
+import About from './components/about'
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <About />
       </div>
     );
   }
---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+
+    return (
+      <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center text-white bg-dark">
+      <h1 className="display-4">About Beau's</h1>
+      <p className="lead">Some text about Beau's.</p>
+    </div>
+    )
+  }
+
+export default About
