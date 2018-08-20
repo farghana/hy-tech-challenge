42ea9fec41c88521dfa06ed34bf484107bb07c30
Farghana Hussain <farghana.hussain@itconquest.com>
2018-08-20 00:18:16 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Beau's App</h1>
+    );
+  }
+}
+
+export default App;
